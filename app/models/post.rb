28c59099b147788759661a21f95ddb634db57f20
yuanxizhang <yuanxi.zhang@gmail.com>
2019-03-12 11:15:51 -0700

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+	validates :title, presence: true
+	validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category , inclusion: { in: ["Fiction", "Non-Fiction"] }
+  validate :title_is_a_clickbait
+
+  def title_is_a_clickbait
+
+    unless title =~ /\b(Won't Believe|Secret|Top \d|Guess)\b/
+      errors.add(:title, " is not a clickbait.")
+    end
+  end
 end
